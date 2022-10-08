e552c3764655c224a6689cc6ec500c6923ca6e1b
Anton Trotsenko <trotsenko.ant07@gmail.com>
2022-10-08 23:09:58 +0200

    Create Controller

---
+++ Lab1/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Design;
+
+namespace Lab1
+{
+    public class Controller
+    {
+        public void CreteTable()
+        { }
+
+        public void OpenTable(string filename) 
+        { }
+    }
+}
