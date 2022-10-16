d770afa96305f3106b063c4a6c36f848f1682cfa
Anton Trotsenko <trotsenko.ant07@gmail.com>
2022-10-16 22:36:58 +0200

    Create SimpleTable

---
+++ Lab1/Models/Tables/SimpleTable.cs
@@ -0,0 +1,26 @@
+﻿using Lab1.Models.Cells;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Models.Tables
+{
+    public class SimpleTable : ITable
+    {
+        private Dictionary<string, SimpleCell> _cells;
+
+        public ICell GetCell(string cell)
+        {
+            if (!_cells.ContainsKey(cell))
+                _cells.Add(cell, new SimpleCell());
+            return _cells[cell];
+        }
+
+        public Dictionary<string, ICell> ExportCells()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
