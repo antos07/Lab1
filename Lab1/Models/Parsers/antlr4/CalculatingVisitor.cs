f5c22f4cd4b83f0f1908b4906333a351614d595b
Anton Trotsenko <trotsenko.ant07@gmail.com>
2022-10-19 00:54:33 +0200

    Define CalculatingVisitor

---
+++ Lab1/Models/Parsers/antlr4/CalculatingVisitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Models.Parsers.antlr4
+{
+    public class CalculatingVisitor : ExpressionsBaseVisitor<dynamic>
+    {
+        
+    }
+}
