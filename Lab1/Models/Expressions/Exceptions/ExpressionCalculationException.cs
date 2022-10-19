d539b8679c9ac6df012646cefb0968c08af101e1
Anton Trotsenko <trotsenko.ant07@gmail.com>
2022-10-19 21:45:32 +0200

    Create base exception for expression calculations

---
+++ Lab1/Models/Expressions/Exceptions/ExpressionCalculationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Models.Expressions.Exceptions
+{
+    public class ExpressionCalculationException : Exception
+    {
+        IExpression Expression { get; }
+        int StartPos { get;}
+        int EndPos { get;}
+
+        public ExpressionCalculationException(string message, IExpression expression, int startPos, int endPos) : base(message)
+        {
+            Expression = expression;
+            StartPos = startPos;
+            EndPos = endPos;
+        }
+    }
+}
