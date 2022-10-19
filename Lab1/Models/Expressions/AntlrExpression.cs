faa2d9a931c0c4ac2c98e9a39b5aac2905031769
Anton Trotsenko <trotsenko.ant07@gmail.com>
2022-10-20 00:40:53 +0200

    Create AntlrExpression

---
+++ Lab1/Models/Expressions/AntlrExpression.cs
@@ -0,0 +1,26 @@
+﻿using Lab1.Models.Parsers.antlr4;
+using Lab1.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Models.Expressions
+{
+    public class AntlrExpression : IExpression
+    {
+        private ExpressionsParser.BooleanExpressionContext _tree;
+
+        public AntlrExpression(ExpressionsParser.BooleanExpressionContext tree)
+        {
+            _tree = tree;
+        }
+
+        public bool Calculate(ITable forTable)
+        {
+            var calculator = new Calculator(forTable);
+            return calculator.Visit(_tree);
+        }
+    }
+}
